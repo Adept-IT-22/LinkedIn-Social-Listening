1686e9128321b605946604d806847bace5eabc7b
Mark Mathenge <mark.mathenge@ADEPT-TECHNO.COM>
2025-01-31 17:34:36 +0300

    to be continued

--- search.py
+++ search.py
@@ -7,14 +7,17 @@ api = Linkedin('markothengo@gmail.com', LINKEDIN_PASSWORD)
 
 search_parameters = ["deepseek", "claude", "llama"]
 
-for parameter in search_parameters:
-    params = {
-        "keywords": parameter,
-        "filters": ""
-        }
-    search = api.search(params, limit = 1)
-    search = json.dumps(search, indent=4)
-    print(search)
-    with open("results.txt", "w") as file:
-        file.write(search)
-    print(f"{parameter} results saved in file.")
+#for parameter in search_parameters:
+params = {
+    "keywords": "call center outsourcing",
+    "includeWebMetadata" : False
+    }
+search = api.search(params, limit = 1, offset = 0)
+summary_search = list(map(lambda x: (x["summary"], x["title"]), search))
+result_list = []
+for item in summary_search:
+    single_post = list(mini_item["text"] for mini_item in item if mini_item is not None)
+    with open("results.txt", "w", encoding="utf-8") as file:
+        file.write(f"{single_post}\n")
+    print("That went well")
+print("Done!")
