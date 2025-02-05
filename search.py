76605b3d43d309fe037620ec2251da8a3eb1d1cb
Mark Mathenge <mark.mathenge@ADEPT-TECHNO.COM>
2025-02-05 17:18:34 +0300

    Store data in dataframe

--- search.py
+++ search.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import os
 import re
 import json
@@ -5,7 +6,7 @@ from linkedin_api import Linkedin
 
 #Create client
 LINKEDIN_PASSWORD = os.environ.get("LINKEDIN_PASSWORD")
-api = Linkedin('markothengo@gmail.com', LINKEDIN_PASSWORD)
+api = Linkedin('suonoventures@gmail.com', "suonoventures")
 
 #Ideal Customer Profile
 icp = ["founder", "ceo", "leader", "manager", "specialist"]
@@ -25,11 +26,14 @@ params = {
 
 #Search for posts based on keywords
 def search_posts(params):
+    print("Search starting...")
     search = api.search(params, limit = 10)
+    print("Search ended!")
     return search
 
 #Get authors
 def get_authors():
+    print("Getting authors...")
     authors = set([])
     start_offset = 0 #where search should start from
 
@@ -47,14 +51,55 @@ def get_authors():
         for post in posts:
             name = post["title"]["text"]
             job = post["primarySubtitle"]["text"]
-            person = name + " - " + job 
+            company = post["actorNavigationUrl"]
+
+            #get urn from actornavigationurl
+            company_urn = re.search(r"(?<=/in/)[^?]+", company)
+            
+            #if urn exists use it to find company name
+            if company_urn:
+                shortened_company_urn = str(company_urn.group(0))
+                company_name = find_company_name(shortened_company_urn)
+            else:
+                company_name = "Company Not Found"
+            
+            #create person variable and add person to authors list
+            person = name + " - " + job + " - " + company_name
             authors.add(person)
+        
+        #change start offset and go again
         start_offset += page_size
-    
+
+    #store info in authors.txt file
+    df_authors = pd.DataFrame(authors)
+    authors_csv_filename = "authors.csv"
+    if os.path.exists(authors_csv_filename):
+        df_authors.to_csv(authors_csv_filename, index=False)
+    else:
+        df_authors.to_csv(authors_csv_filename, mode='a', header=False, index=False)
+
+    print("Authors saved to CSV!")
     return list(authors)
+
+#Find Company Name
+def find_company_name(name: str) -> str:
+    #get company name & location from person's profile
+    individual_profile = api.get_profile(name)
+
+    #company info is located in experience dictionary
+    experience = individual_profile["experience"][0]
+    company_name = experience["companyName"]
+    company_location = experience.get("geoLocationName")
+    if not company_location:
+        company_location = "Location Not Found"
+
+    #return company details
+    company_details = company_name + ", " + company_location
+    return company_details
 
 #Match with ICPs
 def icp_role_match():
+    print("Matching against ICPs...")
     qualified_authors = []
 
     #get all authors
@@ -78,7 +123,18 @@ def icp_role_match():
                 qualified_authors.append(name) #add author to qualified authors list
                 break #stop checking once match is found
 
-    print(qualified_authors)
-    return qualified_authors
+    #save qualified authors in csv
+    df_qualified_authors = pd.DataFrame(qualified_authors)
+
+    #if csv already exists, append. else, create new one
+    qualified_authors_csv_filename = "qualified_authors.csv"
+    if not os.path.exists(qualified_authors_csv_filename):
+        df_qualified_authors.to_csv(qualified_authors_csv_filename, index=False)
+    else:
+        df_qualified_authors.to_csv(qualified_authors_csv_filename, mode='a', header=False, index=False)
+
+    print("Qualified Authors saved to CSV!")
+
+    return df_qualified_authors
 
 icp_role_match()
